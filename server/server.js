c44b246bff13d20832657d09e86fd89a7a42bd66
studmisis <studmisis@bk.ru>
2025-11-24 17:14:05 +0300

    routs in server (#10)

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 import cookieParser from 'cookie-parser';
+import userRouter from './routes/userRouter.js';
+import tokensRouter from './routes/tokensRouter.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -12,6 +14,9 @@ app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/auth', userRouter);
+app.use('/api/tokens', tokensRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
